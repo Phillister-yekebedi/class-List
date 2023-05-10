f52701cf9694228159d2a46583073fd43c50b0a1
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-10 17:25:39 +0300

    uptade

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,15 @@
 fun main() {
     numberInAscendingOrder()
-    allNames("Wycliffe","Yekebedi", "Myles")
-//    val Person = listOf(person("Otieno", 34, 5, 60.5))
-//     val sortedPerson = person.sortedByDescending
-
-    val personName = allNames("Wycliffe","Yekebedi", "Myles")
-    println(personName.toString())
-
-
-
+    println(allNames("Wycliffe","Yekebedi", "Myles").contentToString())
+    var human = listOf(
+        Humans("John", 32,3, 60.5),
+        Humans("Paul",20,3, 50.0),
+        Humans("Dosantos",48,65,78.9)
+        )
+    var sortedHumans = human.sortedByDescending { it.age }
+    println(sortedHumans)
 
 }
-
 //1. Create one function that is given the below array:
 //var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
 //(i) prints out the sum of the second and fifth elements (1 point)
@@ -34,33 +32,26 @@ fun numberInAscendingOrder(){
 
 //2. Given a list of Person objects, each with the attributes, name, age, height, and weight.
 //Sort the list in order of descending age (2 points)
-class person(val name: String, val age: Int, val height: Int, val weight: Double){
+data class Humans(val name: String, val age: Int, val height: Int, val weight: Double){
 
 }
-
-
-
 
 //3. Create a function that takes in 3 names and returns a string array containing all 3 names.
 fun allNames(name1:String, name2: String , name3: String):Array<String>{
-
-    return arrayOf(name1,name2,name3)
-
+    val names = arrayOf(name1,name2,name3)
+    return names
 
 }
 
-
-
-
 //4. Write a function that takes in a list of Car objects each with a registration and mileage
 //attribute and returns the average mileage of all the vehicles in the list. (3 points)
-//class Car (val regestartion: String, val mileage): Double{
-//    fun mileage(cars :List<Car>): Double{
-//        var total = car.fold(0.0){ acc, car ->+car.mileage}
-//    }
-//
-//
-//}
+data class Car (val regestration: String, val mileage): Int{
+    fun mileage(cars :List<Car>): Int{
+        var total = car.fold(0.0){ acc, car ->+car.mileage}
+    }
+
+
+}
 
 
 
